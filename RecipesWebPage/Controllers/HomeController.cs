d3360b54464341d2bf5ac5fd886286910a9f3e57
porterml <90496293+porterml@users.noreply.github.com>
2022-03-25 10:24:36 -0600

    Add project files.

---
+++ RecipesWebPage/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RecipesWebPage.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipesWebPage.Controllers
+{
+    public class HomeController : Controller
+    {
+        private RecipeDbContext _context { get; set; }
+        public HomeController(RecipeDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = _context.Recipes.ToList();
+
+            return View(blah);
+        }
+    }
+}
---
+++ RecipesWebPage/Models/Recipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecipesWebPage.Models
+{
+    public class Recipe
+    {
+        [Key]
+        [Required]
+        public int RecipeID { get; set; }
+        public string RecipeTitle { get; set; }
+        public int RecipeClassID { get; set; }
+        public string Preparation { get; set; }
+        public string Notes { get; set; }
+        
+    }
+}
